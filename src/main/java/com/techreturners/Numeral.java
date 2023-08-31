2889f68f0db4c950f918d56c624136960295badd
Amjad <amjadshakir@gmail.com>
2023-08-31 11:49:10 +0100

    refactor: using enum class for DRY principle

---
+++ src/main/java/com/techreturners/Numeral.java
@@ -0,0 +1,33 @@
+package com.techreturners;
+
+public enum Numeral {
+    THOUSAND(1000,"M"),
+    NINEHUNDRED(900,"CM"),
+    FIVEHUNDRED(500,"D"),
+    FOURHUNDRED(400,"CD"),
+    HUNDRED(100,"C"),
+    NINTHY(90,"XC"),
+    FIFTY(50,"L"),
+    FOURTY(40,"XL"),
+    TEN(10,"X"),
+    NINE(9,"IX"),
+    FIVE(5,"V"),
+    FOUR(4,"IV"),
+    ONE(1,"I");
+
+    private int decimalNumber;
+    private String romanNumeral;
+
+    Numeral(int decimalNumber, String romanNumeral){
+        this.decimalNumber = decimalNumber;
+        this.romanNumeral = romanNumeral;
+    }
+
+    public int getDecimalNumber() {
+        return decimalNumber;
+    }
+
+    public String getRomanNumeral() {
+        return romanNumeral;
+    }
+}
