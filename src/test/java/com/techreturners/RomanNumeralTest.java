27289f92888c277aab73b90b3c24650da2f3651f
Amjad <amjadshakir@gmail.com>
2023-08-31 14:11:37 +0100

    feat: parameterized test from csv source to test

--- src/test/java/com/techreturners/RomanNumeralTest.java
+++ src/test/java/com/techreturners/RomanNumeralTest.java
@@ -2,6 +2,8 @@ package com.techreturners;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -49,4 +51,10 @@ public class RomanNumeralTest {
         assertEquals("MMMCXCVII",romanNumeral.convertDecimalNumberToRomanNumeral(3197));
 
     }
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/input-data-for-decimal-to-roman.csv", numLinesToSkip = 1)
+    public void inputTestDataFromCsvFile(int decimalNumber, String expectedRomanNumeral){
+        assertEquals(expectedRomanNumeral, romanNumeral.convertDecimalNumberToRomanNumeral(decimalNumber));
+    }
+
 }
